f57859532ca09ffa2472a0c6c29502cc6271743b
Njabulo <njabulophiri55@gmail.com>
2025-09-11 08:35:27 +0200

    RabbitMQ exchanges successful

--- aiservice/src/main/java/com/fitness/aiservice/config/RabbitMqConfig.java
+++ aiservice/src/main/java/com/fitness/aiservice/config/RabbitMqConfig.java
@@ -1,4 +1,4 @@
-package com.fitness.activityservice.config;
+package com.fitness.aiservice.config;
 
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
